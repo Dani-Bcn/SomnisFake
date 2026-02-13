3294ed1c96d9505fdc65b8159b56cad43bc454ab
Dani-Bcn <nneodani@gmail.com>
2026-02-13 06:53:19 +0100

    Update Bultex component layout for improved responsiveness

--- src/pages/Bultex.jsx
+++ src/pages/Bultex.jsx
@@ -1,7 +1,7 @@
 export default function Bultex() {
   return (
-    <div className="w-screen px-10 h-screen bg-gray-300 flex flex-col  ">
-      <h2 className="lg:mt-70 mt-40 text-6xl lg:text-9xl bebas-neue-regular">
+    <div className=" bg-gray-300 flex flex-col px-10 ">
+      <h2 className="lg:mt-60 mt-40 text-6xl lg:text-9xl bebas-neue-regular">
         Bultex
       </h2>
       <h3 className="text-4xl lg:text-6xl bebas-neue-regular ">
